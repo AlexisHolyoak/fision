f4ddcb5c290b3a7c69ba3ed897736d651783fefe
Alexis Holyoak <peraltaholyoak.aj@gmail.com>
2018-03-24 11:23:43 -0500

    scripts postgresql sesion2

---
+++ scripts/creacion_tablespace.sql
@@ -0,0 +1,4 @@
+DROP TABLESPACE IF EXISTS ts_data;
+CREATE TABLESPACE ts_data LOCATION 'D:\tablespace/data';
+DROP TABLESPACE IF EXISTS ts_indice;
+CREATE TABLESPACE ts_indice LOCATION 'D:\tablespace\indice';
