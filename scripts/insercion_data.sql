811bbce5ef3c50ced94daed75f40296fd7e55857
Alexis Holyoak <peraltaholyoak.aj@gmail.com>
2018-03-24 14:20:46 -0500

    script de insercion

---
+++ scripts/insercion_data.sql
@@ -0,0 +1,6 @@
+insert into esdbempresarial.TBL_BANCO(codigo,razonsocial) values (1,'SCOTIABANK');
+insert into esdbempresarial.TBL_BANCO(codigo,razonsocial) values (2,'CONTINENTAL');
+
+insert into esdbempresarial.TBL_EMPRESA(codigo,razonsocial) values (1,'GESAP');
+insert into esdbempresarial.TBL_EMPRESA(codigo,razonsocial) values (2,'FISION');
+	
