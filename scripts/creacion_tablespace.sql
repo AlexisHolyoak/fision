a17993f0385e07b0ceebaf3e175be843dd802201
Alexis Holyoak <peraltaholyoak.aj@gmail.com>
2018-03-31 09:11:37 -0500

    scripts

--- scripts/creacion_tablespace.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLESPACE IF EXISTS ts_data;
-CREATE TABLESPACE ts_data LOCATION 'D:\tablespace/data';
-DROP TABLESPACE IF EXISTS ts_indice;
-CREATE TABLESPACE ts_indice LOCATION 'D:\tablespace\indice';
--- scripts/insercion_data.sql
+++
@@ -1,6 +0,0 @@
-insert into esdbempresarial.TBL_BANCO(codigo,razonsocial) values (1,'SCOTIABANK');
-insert into esdbempresarial.TBL_BANCO(codigo,razonsocial) values (2,'CONTINENTAL');
-
-insert into esdbempresarial.TBL_EMPRESA(codigo,razonsocial) values (1,'GESAP');
-insert into esdbempresarial.TBL_EMPRESA(codigo,razonsocial) values (2,'FISION');
-	
